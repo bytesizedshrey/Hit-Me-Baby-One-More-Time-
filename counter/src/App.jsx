5bdb49a483f96d645272b0cb4bb181f629ab4007
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-17 17:16:37 +0530

    Refactor App component for improved readability and functionality; added condition to prevent negative counter values and standardized code formatting.

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [counter, setcounter] = useState(0) //read , write
+  const [counter, setcounter] = useState(0); //read , write
 
   const addValue = () => {
     // counter = counter + 1
-    setcounter(counter + 1)
-  }
+    setcounter(counter + 1);
+  };
 
   const decreaseValue = () => {
-    setcounter(counter - 1)
-  }
-  
+    if (counter > 0) {
+      setcounter(counter - 1);
+    }
+  };
+
   return (
     <>
-        <h1>Chai or React</h1>
-        <h2>Counter Value : ${counter}</h2>
+      <h1>Chai or React</h1>
+      <h2>Counter Value : ${counter}</h2>
 
-        <button
-        onClick={addValue}
-        >Add Value</button>
-        <br />
-        <button
-        onClick={decreaseValue}
-        >Decrease Value</button>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={decreaseValue}>Decrease Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
